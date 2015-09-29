1389ac241fdb80a2b2f8196409114d0a2193ebe3
xiang.yu <xiang.yu@17daxue.com>
2015-09-29 17:49:14 +0800

    重命名resume

---
+++ webservant/webpage/src/main/java/com/xyu/web/cover/action/CoverController.java
@@ -0,0 +1,17 @@
+package com.xyu.web.cover.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Xiang on 2015/9/29.
+ */
+@Controller
+@RequestMapping(value = "cover")
+public class CoverController {
+
+    @RequestMapping(value="index")
+    public String index(){
+        return "cover/cover-index";
+    }
+}
