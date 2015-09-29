b00b338650f034907e45e2fc319faf07f59f994e
xiang.yu <xiang.yu@17daxue.com>
2015-09-29 17:50:08 +0800

    重命名resume

---
+++ webservant/webpage/src/main/java/com/xyu/web/resume/action/ResumeController.java
@@ -0,0 +1,18 @@
+package com.xyu.web.resume.action;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Xiang on 2015/9/13.
+ */
+@Controller
+@RequestMapping(value = "resume")
+public class ResumeController {
+
+    @RequestMapping(value="index")
+    public String index(){
+        return "resume/resume-index";
+    }
+}
